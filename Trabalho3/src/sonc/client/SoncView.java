0c69aea3787fdfdb5043f461740496be0bbd3735
RicardoGPP <ricardo.piantavinha@gmail.com>
2018-05-22 15:08:14 +0100

    Remoção de comentário da classe SoncView.

--- Trabalho3/src/sonc/client/SoncView.java
+++ Trabalho3/src/sonc/client/SoncView.java
@@ -1,13 +1,13 @@
 package sonc.client;
 
 import com.google.gwt.core.client.EntryPoint;
-//import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class SoncView implements EntryPoint {
 
-	//private final SoncServiceAsync service = GWT.create(SoncService.class);
+	private final SoncServiceAsync service = GWT.create(SoncService.class);
 	
 	public void onModuleLoad() {
 		RootPanel rootPanel = RootPanel.get();
